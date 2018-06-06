5ba3b521b847d680e8bbea0ba22bd3518ae7a17f
Alan Cooper <alan@avontoun.co.uk>
2018-06-06 11:57:58 +0100

    Maing current state of play available

--- roles/logstash/files/S3pull.sh
+++ roles/logstash/files/S3pull.sh
@@ -11,10 +11,10 @@ while read p; do
 done <filelist
 
 
-aws s3 ls s3://akamai-lds-ftp-ldslogss3bucket-ydfvjkr96ig/mobile_ds_skyscanner_net_115834/"$(date --date=yesterday +"%Y/%m/")" | grep "$(date --date=yesterday +"%Y%m%d")" | awk '{print $4}' >filelist
-
-while read p; do
-    echo $p
-    aws s3 cp s3://akamai-lds-ftp-ldslogss3bucket-ydfvjkr96ig/mobile_ds_skyscanner_net_115834/"$(date --date=yesterday +"%Y/%m")"/$p /mnt/logs
-    gunzip /mnt/logs/$p
-done <filelist
+#aws s3 ls s3://akamai-lds-ftp-ldslogss3bucket-ydfvjkr96ig/mobile_ds_skyscanner_net_115834/"$(date --date=yesterday +"%Y/%m/")" | grep "$(date --date=yesterday +"%Y%m%d")" | awk '{print $4}' >filelist
+#
+#while read p; do
+#    echo $p
+#    aws s3 cp s3://akamai-lds-ftp-ldslogss3bucket-ydfvjkr96ig/mobile_ds_skyscanner_net_115834/"$(date --date=yesterday +"%Y/%m")"/$p /mnt/logs
+#    gunzip /mnt/logs/$p
+#done <filelist
